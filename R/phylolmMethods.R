93f1ac8cff143daf54823b0405a583e46a99ccfd
Paul Bastide <paul.bastide@m4x.org>
2023-04-24 22:16:27 +0200

    Typos.

--- R/phylolmMethods.R
+++ R/phylolmMethods.R
@@ -21,8 +21,82 @@ getTree <- function(cdata) {
   } else {
     tree <- phylo.tree(cdata)
     if (!ape::is.ultrametric(tree)) stop("The tree must be ultrametric.")
+    if (length(tree$tip.label) != ncol(count.matrix(cdata))) {
+      sample.annotations <- sample.annotations(cdata)
+      sample.annotations$id <- colnames(count.matrix(cdata))
+      sample.annotations$id.species <- tree$tip.label[sample.annotations$id.species]
+      tree <- addReplicatesOnTree(tree, sample.annotations, species = "id.species", id = "id")
+    }
     return(tree)
   }
+}
+
+#' @title Add replicates to a tree
+#'
+#' @description
+#' Utility function to add replicates to a tree, as tips with zero length branches.
+#'
+#' @param tree A phylogenetic tree with n tips.
+#' @param traits A data frame containing at least two columns,
+#' one with sample ids, and on with species names for each samples.
+#' @param species Name of the column containing species names. Default to "species".
+#' @param id Name of the column containing samples ids. Default to "id". If "auto", the identifiers are automatically generated from the species names.
+#' @param eps A small number to add to terminal branch lengths to avoid true zeros. Default to \code{.Machine$double.eps}.
+#'
+#' @return A phylogenetic tree with as many tips as the number of rows in \code{traits},
+#'  and clusters of tips with zero branch lengths corresponding to replicates.
+#'
+#' @keyword internal
+#'
+addReplicatesOnTree <- function(tree, traits,
+                                species = "species", id = "id",
+                                eps = .Machine$double.eps^2) {
+  if (!requireNamespace("phytools", quietly = TRUE)) {
+    stop("Package 'phytools' is needed for function 'add_replicates'.", call. = FALSE)
+  }
+  if (!is.null(traits[[id]]) && (length(traits[[id]]) != length(unique(traits[[id]])))){
+    stop("The samples ids in column named ", id, " should be unique identifiers of the samples.")
+  }
+  if (is.null(traits[[species]])) {
+    stop("The `traits` data frame should contain a column named ", species, " with species names for each sample. Plead adjust argument `species` accordingly.")
+  }
+  if (is.null(traits[[id]]) && id != "auto") {
+    stop("The `traits` data frame should contain a column named ", id, " with sample ids. Plead adjust argument `id` accordingly.")
+  }
+  if (is.null(traits[[id]]) && id == "auto") {
+    id <- "id"
+    traits[[id]] <- make.unique(traits[[species]], sep = "_")
+  }
+  data_tree_cor <- match(traits[[species]], tree$tip.label)
+  if (anyNA(data_tree_cor)) {
+    # Species in data NOT in the tree
+    stop("Species '", paste(unique(traits[[species]][is.na(data_tree_cor)]), collapse = "', '"), "' are in the data but not in the tree. Please remove them from the data before proceding (or use a tree that include them)." )
+  }
+  tree_data_cor <- match(tree$tip.label, traits[[species]])
+  if (anyNA(tree_data_cor)) {
+    # Species in tree NOT in the data
+    warning("Species '", paste(unique(tree$tip.label[is.na(tree_data_cor)]), collapse = "', '"), "' are in the tree but not in the data. They will be droped from the final tree." )
+  }
+  ## Unique id for species
+  id_species <- paste0(traits[[species]], tempfile(pattern = "tmp", tmpdir = ""))
+  traits[[species]] <- id_species
+  tree$tip.label <- id_species[tree_data_cor]
+  ## Make tree
+  tree_rep <- tree
+  # Add replicates
+  for (tip_label in tree$tip.label) {
+    all_rep_ids <- traits[[id]][traits[[species]] == tip_label]
+    for (rep_id in all_rep_ids) {
+      tree_rep <- phytools::bind.tip(tree_rep, tip.label = rep_id,
+                                     where = which(tree_rep$tip.label == tip_label))
+    }
+  }
+  # Remove original tips
+  tree_rep <- ape::drop.tip(tree_rep, tree$tip.label)
+  # No true zeros
+  tree_rep$edge.length[tree_rep$edge[, 2] %in% 1:length(tree_rep$tip.label)] <- tree_rep$edge.length[tree_rep$edge[, 2] %in% 1:length(tree_rep$tip.label)] + eps
+  # result
+  return(tree_rep)
 }
 
 #' @title Extract phylolm results
@@ -254,10 +328,11 @@ phylolm.createRmd <- function(data.path, result.path, codefile,
   extra_args <- eval(substitute(alist(...)))
   extra_args <- sapply(extra_args, function(x) paste(" = ", x))
   extra_args <- paste(names(extra_args), extra_args, collapse = ", ")
-  writeLines(c("tree <- getTree(cdata)"),codefile)
-  writeLines(c("min_sigma2_error <- (.Machine$double.eps)^0.5 * max(ape::node.depth.edgelength(phy))"),codefile)
+  if (length(extra_args) > 1) extra_args <- paste0(", ", extra_args)
+  writeLines(c("tree <- getTree(cdata)"), codefile)
+  writeLines(c("min_sigma2_error <- (.Machine$double.eps)^0.5 * max(ape::node.depth.edgelength(tree))"),codefile)
   writeLines(c(
-    paste0("phylolm.results_list <- apply(data.trans, 1, phylolm_analysis, design_data = design_data, design_formula = design_formula, tree = tree, model = '", model, "', measurement_error = ", measurement_error, ", lower.bound = list(sigma2_error = min_sigma2_error))", ", ", extra_args, ")"),
+    paste0("phylolm.results_list <- apply(data.trans, 1, phylolm_analysis, design_data = design_data, design_formula = design_formula, tree = tree, model = '", model, "', measurement_error = ", measurement_error, ", lower.bound = list(sigma2_error = min_sigma2_error)", extra_args, ")"),
     "result.table <- do.call(rbind, phylolm.results_list)"),
     codefile)
   writeLines(c(
